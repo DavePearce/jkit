cfcff935ef17e6f50b1c95c20442ff50e7c3e930
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-07-07 06:02:10 +0000

    Improved the bypass methods test case.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8026 95d365e6-2d05-0410-86de-9ddc6db2c6c8

--- tests/jkit/src/JLS_XXX_BypassMethod_4.java
+++ tests/jkit/src/JLS_XXX_BypassMethod_4.java
@@ -1,18 +1,26 @@
 public class JLS_XXX_BypassMethod_4 {
     public static interface Inter {
-	public Object f();
+	public Number f();
     }
 
     public static class Child implements Inter {
-	public String f() {
-	    return "Hello World";
+	public Integer f() {
+	    return 12321;
 	}
+    }
+
+    public static void p1(Integer i) {
+	System.out.println("GOT INTEGER: " + i);
+    }
+
+    public static void p2(Number i) {
+	System.out.println("GOT NUMBER: " + i);
     }
 
     public static void main(String[] args) {
 	JLS_XXX_BypassMethod_4.Child tc = new JLS_XXX_BypassMethod_4.Child();
 	JLS_XXX_BypassMethod_4.Inter ti = tc;
-	System.out.println(tc.f());
-	System.out.println(ti.f());	    
+	p1(tc.f());
+	p2(ti.f());
     }
 }
