59d52e60501d99dd99e18a0cdc34e3c6673355c2
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-07-09 05:08:16 +0000

    More licenses.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8059 95d365e6-2d05-0410-86de-9ddc6db2c6c8

--- jkit/util/Triple.java
+++ jkit/util/Triple.java
@@ -17,7 +17,7 @@
 // write to the Free Software Foundation, Inc., 59 Temple Place, 
 // Suite 330, Boston, MA  02111-1307  USA
 //
-// (C) David James Pearce, 2007. 
+// (C) David James Pearce, 2009. 
 
 package jkit.util;
 
