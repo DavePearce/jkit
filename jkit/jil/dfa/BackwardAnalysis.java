9640231cc22a157ea787544ae7be2a32faf783c2
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-07-09 05:10:17 +0000

    More reshuffling.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8061 95d365e6-2d05-0410-86de-9ddc6db2c6c8

--- jkit/jil/dfa/BackwardAnalysis.java
+++ jkit/jil/dfa/BackwardAnalysis.java
@@ -17,10 +17,9 @@
 // write to the Free Software Foundation, Inc., 59 Temple Place, 
 // Suite 330, Boston, MA  02111-1307  USA
 //
-// (C) Constantine Dymnikov, 2008. 
+// (C) Constantine Dymnikov, 2009. 
 
-
-package jkit.util.dfa;
+package jkit.jil.dfa;
 
 import java.util.Collection;
 import java.util.Collections;
