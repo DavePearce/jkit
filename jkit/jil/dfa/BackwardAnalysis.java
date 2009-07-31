f3cd7845041b4cb2ea859f147e37c6de25000a06
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-07-31 05:11:13 +0000

    Bug fix for eliminating notes from conditions.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8271 95d365e6-2d05-0410-86de-9ddc6db2c6c8

--- jkit/jil/dfa/BackwardAnalysis.java
+++ jkit/jil/dfa/BackwardAnalysis.java
@@ -89,9 +89,10 @@ public abstract class BackwardAnalysis<T extends FlowSet> {
 				merge(current,store,worklist,preds);
 			} else if(stmt instanceof JilStmt.IfGoto) {				
 				JilStmt.IfGoto gto = (JilStmt.IfGoto) stmt;
+				JilExpr notCondition = Exprs.eliminateNot(new JilExpr.UnOp(gto.condition(), JilExpr.UnOp.NOT,Types.T_BOOL));
 				int target = labels.get(gto.label());
 				T t_store = transfer(gto.condition(),get_store(target,emptyStore));
-				T f_store = transfer(new JilExpr.UnOp(gto.condition(), JilExpr.UnOp.NOT,Types.T_BOOL),get_store(current+1,emptyStore));				
+				T f_store = transfer(notCondition,get_store(current+1,emptyStore));				
 				T store = join(t_store,f_store);				
 				merge(current,store,worklist,preds);				
 			} else if(stmt instanceof JilStmt.Switch) {
