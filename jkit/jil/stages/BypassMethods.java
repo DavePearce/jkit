780db26ed28415863fd5b0b4b2be5b25c4e8e970
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-08-20 04:12:06 +0000

    Possible bug fix for bypass methods.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8348 95d365e6-2d05-0410-86de-9ddc6db2c6c8

--- jkit/jil/stages/BypassMethods.java
+++ jkit/jil/stages/BypassMethods.java
@@ -125,7 +125,8 @@ public final class BypassMethods {
 			Type mtReturnType = stripGenerics(mt.returnType());
 			
 			boolean isMatch = ft.returnType() instanceof Type.Variable
-					|| (!ftReturnType.equals(mtReturnType));
+					&& (!ftReturnType.equals(mtReturnType));
+			
 			for (int i = 0; i != ftParamTypes.size(); ++i) {
 				Type fp = ftParamTypes.get(i);
 				Type mp = mtParamTypes.get(i);
@@ -135,7 +136,7 @@ public final class BypassMethods {
 						| (fp instanceof Type.Variable && !(mp instanceof Type.Variable));
 			}
 
-			if (isMatch) {							
+			if (isMatch) {				
 				problems.add(new Triple<Clazz, Clazz.Method, Type.Function>(minfo
 						.first(), minfo.second(), method.type()));
 			}
