33dde4775f7a9af42094377d191ff3415d3e1fd8
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-08-26 06:12:35 +0000

    Minor bug fixes.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8383 95d365e6-2d05-0410-86de-9ddc6db2c6c8

--- tests/jkit/src/JLS_9_3_ConstantFields_2.java
+++ tests/jkit/src/JLS_9_3_ConstantFields_2.java
@@ -9,7 +9,7 @@ public class JLS_9_3_ConstantFields_2 {
     public static interface Inter extends aInter, bInter {
     }
 
-    public static class Inner extends JLS_9_3_ConstantFields_1 implements Inter {
+    public static class Inner extends JLS_9_3_ConstantFields_2 implements Inter {
     }
 
     public static void main(String[] args) {
