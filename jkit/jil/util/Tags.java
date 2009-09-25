6d7dfd4b90aba18830a78dbda32c20a143087165
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-09-25 03:06:37 +0000

    Added tags class.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8617 95d365e6-2d05-0410-86de-9ddc6db2c6c8

---
+++ jkit/jil/util/Tags.java
@@ -0,0 +1,56 @@
+package jkit.jil.util;
+
+import jkit.jil.tree.Type;
+import jkit.util.Pair;
+import jkit.util.Triple;
+
+/**
+ * The purpose of tags is to uniquely identify methods and fields in a given
+ * Java program. Primarily, this is useful for the interprocedural analysis
+ * phases.
+ * 
+ * @author djp
+ * 
+ */
+public class Tags {
+	
+	public static class Method extends Triple<Type.Clazz, String, Type.Function> {
+		public Method(Type.Clazz owner, String name, Type.Function type) {
+			super(owner, name, type);
+		}
+
+		public Type.Clazz owner() {
+			return first();
+		}
+
+		public String name() {
+			return second();
+		}
+
+		public Type.Function type() {
+			return third();
+		}
+		
+		public String toString() {
+			return first() + "." + second() + ":" + type();
+		}		
+	}
+	
+	public static class Field extends Pair<Type.Clazz, String> {
+		public Field(Type.Clazz owner, String name) {
+			super(owner, name);
+		}
+
+		public Type.Clazz owner() {
+			return first();
+		}
+
+		public String name() {
+			return second();
+		}
+		
+		public String toString() {
+			return first() + "." + second();
+		}		
+	}
+}
