3ae93f67e703a23373ad34863aa4a9c3e3f8b0b5
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-10-22 21:53:06 +0000

    Added another test case.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8849 95d365e6-2d05-0410-86de-9ddc6db2c6c8

--- jkit/testing/tests/JKit.java
+++ jkit/testing/tests/JKit.java
@@ -58,7 +58,8 @@ public class JKit extends TestHarness {
 	@Ignore("Known Bug") @Test public void TypeBounds_9() { compileFailTest("TypeBounds_9"); }
 	@Ignore("Known Bug") @Test public void TypeBounds_10() { compileFailTest("TypeBounds_10"); }
 	@Test public void TypeBounds_11() { runTest("TypeBounds_11"); }
-	
+	@Test public void TypeBounds_12() { runTest("TypeBounds_12"); }
+		
 	@Test public void WhileLoop_1() { runTest("WhileLoop_1"); }
 
 	@Test public void Imports_1() { runTest("Imports_1"); }
