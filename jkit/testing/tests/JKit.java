ba88c89fa68ac8f8b1d3379409a0133f75ae7555
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-10-27 22:47:56 +0000

    Added yet another test case!
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8861 95d365e6-2d05-0410-86de-9ddc6db2c6c8

--- jkit/testing/tests/JKit.java
+++ jkit/testing/tests/JKit.java
@@ -109,6 +109,7 @@ public class JKit extends TestHarness {
 	@Test public void JLS_8_1_2_GenericTypes_11() { runTest("JLS_8_1_2_GenericTypes_11"); }
 	@Ignore("Known bug") @Test public void JLS_8_1_2_GenericTypes_12() { runTest("JLS_8_1_2_GenericTypes_12"); }
 	@Test public void JLS_8_1_2_GenericTypes_13() { runTest("JLS_8_1_2_GenericTypes_13"); }
+	@Test public void JLS_8_1_2_GenericTypes_14() { runTest("JLS_8_1_2_GenericTypes_14"); }
 	
 	@Test public void JLS_8_1_3_InnerClasses_1() { runTest("JLS_8_1_3_InnerClasses_1"); }
 	@Test public void JLS_8_1_3_InnerClasses_2() { runTest("JLS_8_1_3_InnerClasses_2"); }
