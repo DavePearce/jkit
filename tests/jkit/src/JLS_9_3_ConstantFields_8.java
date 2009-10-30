8a1c3cd7f13b43296c5b096cd06129d961580b94
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2009-10-30 04:06:29 +0000

    Added another test case.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@8895 95d365e6-2d05-0410-86de-9ddc6db2c6c8

---
+++ tests/jkit/src/JLS_9_3_ConstantFields_8.java
@@ -0,0 +1,7 @@
+public class JLS_9_3_ConstantFields_8 {
+    public static final Test CONSTANT = null;
+
+    public static void main(String[] args) {
+	System.out.println("GOT " + CONSTANT);
+    }
+}
