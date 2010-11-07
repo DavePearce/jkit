a04c371c268cc9c38427b6b082f73fc8cc4d2902
DavidPearce <david.pearce@ecs.vuw.ac.nz>
2010-11-07 21:59:18 +0000

    Added yet another test case arising from the jvm98 raytracer.
    git-svn-id: svn+ssh://barretts.ecs.vuw.ac.nz/vol/projects/elvis-svn/jkit/src@11114 95d365e6-2d05-0410-86de-9ddc6db2c6c8

--- jkit/testing/tests/JKit.java
+++ jkit/testing/tests/JKit.java
@@ -248,6 +248,7 @@ public class JKit extends TestHarness {
 	@Test public void JLS_14_14_ForStatement_9() { runTest("JLS_14_14_ForStatement_9"); }
 	
 	@Test public void JLS_14_19_Synchronization_1() { runTest("JLS_14_19_Synchronization_1"); }
+	@Test public void JLS_14_19_Synchronization_2() { runTest("JLS_14_19_Synchronization_2"); }
 	
 	@Test public void JLS_14_20_TryStatement_1() { runTest("JLS_14_20_TryStatement_1"); }
 	@Test public void JLS_14_20_TryStatement_2() { runTest("JLS_14_20_TryStatement_2"); }
